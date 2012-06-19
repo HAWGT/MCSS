84ae358a1c8c0a9625ac2df81517f1e67fb77f6e
R4T H4CK3R <r4th4ck3r@gmail.com>
2012-06-19 19:18:49 +0100

    Step #2
    Change Config.php

--- Config.php
+++ Config.php
@@ -1,7 +1,8 @@
 <?php
+//You need a database called minecraft with a table called users, with username and hashed(sha 256) password
 $host="localhost";
 $username="root";
-$password="darthvader";
+$password="";
 $db_name="minecraft";
 $tbl_name="users";
 ?>
